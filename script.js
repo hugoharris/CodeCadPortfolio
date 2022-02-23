9e7514843a5a90e2bbc09f88e4975edb2e229d71
Kevin Harris <hugokharris@gmail.com>
2022-02-23 14:44:14 -0700

    clean up JavaScript to eliminate global variables

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
-// global variables
+// textControl function eliminates need for global variables
 
-const aboutText = document.getElementById('about-text');
-const portfolioText = document.getElementById('portfolio-text');
-const contactText = document.getElementById('contact-text');
+function textControl() {
+  // variables
 
-const about = document.getElementsByClassName('about');
-const portfolio = document.getElementsByClassName('portfolio');
-const contact = document.getElementsByClassName('contact');
+  const aboutText = document.getElementById("about-text");
+  const portfolioText = document.getElementById("portfolio-text");
+  const contactText = document.getElementById("contact-text");
 
-// text change functions
+  const about = document.getElementsByClassName("about");
+  const portfolio = document.getElementsByClassName("portfolio");
+  const contact = document.getElementsByClassName("contact");
 
-function changeAbout() {
-    aboutText.style.display = 'block';
-    portfolioText.style.display = 'none';
-    contactText.style.display = 'none';
+  // text change functions
+
+  function changeAbout() {
+    aboutText.style.display = "block";
+    portfolioText.style.display = "none";
+    contactText.style.display = "none";
+  }
+
+  function changePortfolio() {
+    aboutText.style.display = "none";
+    portfolioText.style.display = "block";
+    contactText.style.display = "none";
+  }
+  function changeContact() {
+    aboutText.style.display = "none";
+    portfolioText.style.display = "none";
+    contactText.style.display = "block";
+  }
+
+  // add event listeners
+
+  about[0].addEventListener("click", changeAbout);
+  portfolio[0].addEventListener("click", changePortfolio);
+  contact[0].addEventListener("click", changeContact);
 }
 
-function changePortfolio() {
-    aboutText.style.display = 'none';
-    portfolioText.style.display = 'block';
-    contactText.style.display = 'none';
-}
-function changeContact() {
-    aboutText.style.display = 'none';
-    portfolioText.style.display = 'none';
-    contactText.style.display = 'block';
-}
+// fire the textControl function
 
-// add event listeners
-
-about[0].addEventListener('click', changeAbout);
-portfolio[0].addEventListener('click', changePortfolio);
-contact[0].addEventListener('click', changeContact);
+textControl();
